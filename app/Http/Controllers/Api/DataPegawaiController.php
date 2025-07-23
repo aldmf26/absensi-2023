fe84c40a876e7e83fe8ca156abb08b385c9b714d
aldmf26 <aldimf26@gmail.com>
2025-07-23 13:32:42 +0800

    dsa

--- app/Http/Controllers/Api/DataPegawaiController.php
+++ app/Http/Controllers/Api/DataPegawaiController.php
@@ -40,7 +40,7 @@ class DataPegawaiController extends Controller
                 'a.ket'
             )
             ->whereYear('a.tanggal', date('Y'))
-            ->where('k.id_karyawan', $nama)
+            ->where('k.nama_karyawan', $nama)
             ->get();
 
         $datas = [
